b9e5eaf4111bc8d384688f284d2c76d46efb5c48
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-31 15:16:06 +1100

    (minor): comments

--- ActivitySelectionProblem/Regular.cs
+++ ActivitySelectionProblem/Regular.cs
@@ -2,7 +2,7 @@
 
 namespace ActivitySelectionProblem
 {
-    // The activity selection problem as described at: https://en.wikipedia.org/wiki/Activity_selection_problem
+    // The activity selection problem greedy algorithm as described at: https://en.wikipedia.org/wiki/Activity_selection_problem
     public static class Regular
     {
         public static int Compute(params (int, int)[] activities) =>
